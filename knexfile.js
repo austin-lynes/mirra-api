21fab23dd860c6e69f6f44845a4c4411cb065bdf
AustinLynes <austinlynes94@gmail.com>
2020-04-04 13:21:58 -0700

    addding more changes

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,6 @@ module.exports = {
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       directory: './data/migrations',
     },
@@ -17,10 +13,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       directory: './data/migrations',
     },
