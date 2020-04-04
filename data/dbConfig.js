624c02bc4d89100a685dbf603a7c76ad0612cdd7
AustinLynes <austinlynes94@gmail.com>
2020-04-04 13:24:24 -0700

    change env var

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 
 const config = require('../knexfile.js');
 
-const dbEnv = process.env.DB_ENV || 'development';
+const dbEnv = process.env.NODE_ENV || 'development';
 
 const db =  knex(config[dbEnv])
 
